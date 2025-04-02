c679e1fd3b2555fa91b182e685d8918730e3a609
Karl <karlcampbell100@gmail.com>
2025-04-02 21:12:51 +0100

    complete handleFileChange that allows for user uploaded mp3s to be added to the audio controller

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 Visualise is an online audio visualiser that will allow users to upload and play their own songs. This will be accompanied by dynamic live visuals synced to the audio. Users will be able to select a themee / mode (bars, waves etc) and their preferences will be saved. The site utilises a preset list of royalty free music and they will be able to search & filter these by genre / mood. 
 
 ### Key Features (Front)
-- [ ] Upload & Play Songs
+- [x] Upload & Play Songs
 - [ ] Live Visuals (animated visualiser synced to audio)
 - [ ] Theme / Mode Selection
 - [ ] User Preferences
@@ -16,9 +16,9 @@ Visualise is an online audio visualiser that will allow users to upload and play
 - [ ] visualiser settings (styles, colors and effects)
 
 ### Plan (Front)
-- [ ] Play / pause button
-- [ ] Upload tracks button (loaded into `<audio>`)
+- [x] Play / pause button
+- [x] Upload tracks button (loaded into `<audio>`)
 - [ ] Canvas (for visualiser)
 - [ ] Theme selection
-- [ ] Song progress bar
+- [x] Song progress bar
 - [ ] Song search page
--- src/player.tsx
+++ src/player.tsx
@@ -1,14 +1,38 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState, ChangeEvent, ChangeEventHandler } from 'react'
+import React from 'react';
 import "./player.css"
 
+
 export const Player = () =>{
+
+const [audioSrc, setAudioSrc] = useState<string | null>(null);
+
+const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
+    const file = event.target.files?.[0];
+    if (file){
+        const fileUrl = URL.createObjectURL(file);
+        setAudioSrc(fileUrl);
+    }
+
+
+// const audioCtx = new AudioContext();
+// const analyser = audioCtx.createAnalyser();
+
+// const source = audioCtx.createMediaStreamSource(file)
+// source.connect(analyser);
+// analyser.connect(distortion);
+// dist
+
+}
+
     return(
         <div id='player-holder'>
-            <input type="file" id='song' accept='.mp3'/>
-            <div id='progress-bar'></div>
-            <span id='song-title'>Song Title</span>
-            <span id='artist-name'>Artist Name</span>
-            <img src="../src/assets/play-button.svg" alt="Pause Button" id='sound-button'/>
+            <input type="file" id='song' accept='.mp3'onChange={handleFileChange}/>
+            <audio src={audioSrc ?? undefined} loop controls></audio>
+            {/* <div id='progress-bar'></div> */}
+            {/* <span id='song-title'>Song Title</span>
+            <span id='artist-name'>Artist Name</span> */}
+            {/* <img src="../src/assets/play-button.svg" alt="Pause Button" id='sound-button'/> */}
         </div>
     )
 }
