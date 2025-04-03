a78e11f52787bfacf4dbaf29004c4485f23dbae3
Karl <139282956+ksuko1@users.noreply.github.com>
2025-04-03 13:25:56 +0100

    using p5.js, create a canvas with a circle ready for audio manipulation

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+// import p5 from 'p5';
 import { Player } from './player'
+
+
 
 
 createRoot(document.getElementById('root')!).render(
--- src/player.tsx
+++ src/player.tsx
@@ -1,4 +1,5 @@
-import { StrictMode, useState, ChangeEvent, ChangeEventHandler } from 'react'
+import p5 from 'p5';
+import { StrictMode, useState, ChangeEvent, ChangeEventHandler, useEffect, useRef} from 'react'
 import React from 'react';
 import "./player.css"
 
@@ -6,6 +7,25 @@ import "./player.css"
 export const Player = () =>{
 
 const [audioSrc, setAudioSrc] = useState<string | null>(null);
+const canvasRef = useRef(null);
+
+useEffect(() =>{
+    const sketch = (p5) =>{
+
+        p5.setup = () =>{
+            p5.createCanvas(500,500).parent(canvasRef.current)
+        };
+
+        p5.draw = () =>{
+            p5.background(200);
+            p5.fill(0,0,255);
+            p5.ellipse(50,50,80);
+        };
+    };
+        new p5(sketch);
+
+        return () => {}
+}, []);
 
 const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
     const file = event.target.files?.[0];
@@ -14,21 +34,13 @@ const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
         setAudioSrc(fileUrl);
     }
 
-
-// const audioCtx = new AudioContext();
-// const analyser = audioCtx.createAnalyser();
-
-// const source = audioCtx.createMediaStreamSource(file)
-// source.connect(analyser);
-// analyser.connect(distortion);
-// dist
-
 }
 
     return(
         <div id='player-holder'>
             <input type="file" id='song' accept='.mp3'onChange={handleFileChange}/>
             <audio src={audioSrc ?? undefined} loop controls></audio>
+            <div id='canvas-container' ref={canvasRef}></div>
             {/* <div id='progress-bar'></div> */}
             {/* <span id='song-title'>Song Title</span>
             <span id='artist-name'>Artist Name</span> */}
